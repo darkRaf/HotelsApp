3f24e366aaa0148474fc6dc105762ced632d2a30
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2022-11-30 23:14:20 +0100

    Hotels PureComponent and useMemo

--- src/componets/Hotels/Hotels.js
+++ src/componets/Hotels/Hotels.js
@@ -1,26 +1,55 @@
-import { Component } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Hotel from './Hotel/Hotel';
 import styles from './Hotels.module.css';
 
 const propTypes = {
-  hotels: PropTypes.array.isRequired
-}
+  hotels: PropTypes.array.isRequired,
+};
 
-class Hotels extends Component {
-  componentWillUnmount() {}
-
-  render() {
-    return (
-      <div className={`${styles.container} container`}>
-        <h2 className={styles.title}>Oferty</h2>
-        {this.props.hotels.map(hotel => (
-          <Hotel key={hotel.id} {...hotel}  theme={this.props.theme} />
-        ))}
-      </div>
-    );
-  }
+const slowFunction = (count) => {
+  for (let i = 0; i < 1000000000; i++) {}
+  return count;
 }
+function Hotels(props) {
+
+  useEffect(() => console.log('hotels render'));
+
+  const count = useMemo(() => {
+    return slowFunction(props.hotels.length);
+  }, [props.hotels.length]);
+
+  return (
+    <div className={`${styles.container} container`}>
+      <h2 className={styles.title}>Oferty ({count})</h2>
+      {props.hotels.map(hotel => (
+        <Hotel key={hotel.id} {...hotel} theme={props.theme} />
+      ))}
+    </div>
+  );
+}
+// class Hotels extends PureComponent {
+//   componentWillUnmount() {}
+
+//   componentDidUpdate() {
+//     console.log('Hotles render');
+//   }
+
+// shouldComponentUpdate(nextProps, nextState) {
+//   return this.props.hotels !== nextProps.hotels;
+// }
+
+//   render() {
+//     return (
+//       <div className={`${styles.container} container`}>
+//         <h2 className={styles.title}>Oferty</h2>
+//         {this.props.hotels.map(hotel => (
+//           <Hotel key={hotel.id} {...hotel}  theme={this.props.theme} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 // function Hotels() {
 //   return (
@@ -32,4 +61,9 @@ class Hotels extends Component {
 
 Hotels.propTypes = propTypes;
 
+const areEquall = (prevProps, nextProps) => {
+  return prevProps.hotels === nextProps.hotels;
+}
+
 export default Hotels;
+// export default React.memo(Hotels, areEquall);
